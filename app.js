ebab429363b2341647cb759cd2cfe0a0bef4cc06
DerQh <sanafredrick@gmail.com>
2023-08-03 20:12:06 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const section_OneEl = document.querySelector(".one");
+const navbarEL = document.querySelector(".navbar");
+const wrappperDivEl = document.querySelectorAll(".wrapper_div");
+
+function scroll_naavbar() {
+  window.onscroll = function () {
+    if (window.pageYOffset >= navbarEL.offsetTop) {
+      section_OneEl.classList.add("sticky");
+    } else {
+      section_OneEl.classList.remove("sticky");
+    }
+  };
+}
+
+function observeDivs() {
+  // create observer//
+  const callback_Func = function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry);
+        entry.target.classList.add("scroll");
+      } else {
+        // entry.target.classList.add("scroll");
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callback_Func, {
+    threshold: 0.6,
+    rootMargin: "60px",
+  });
+
+  // loop over enties//
+  wrappperDivEl.forEach((div) => {
+    observer.observe(div);
+  });
+}
+
+scroll_naavbar();
+observeDivs();
