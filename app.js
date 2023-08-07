4ceba85006e94cea75b268b41ad81b8cb5a23164
DerQh <sanafredrick@gmail.com>
2023-08-07 22:07:42 +0300

    others

--- app.js
+++ app.js
@@ -37,11 +37,9 @@ function observeDivs() {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         if (num % 2 === 0) {
-          console.log(entry, num);
           entry.target.classList.add("scroll");
           entry.target.classList.add("fadeinTopRight");
         } else {
-          console.log(entry, num);
           entry.target.classList.add("scroll");
           entry.target.classList.add("fadeinTop");
         }
@@ -109,10 +107,6 @@ function toggelMenu() {
       el.classList.toggle("aqua_backg");
     });
   });
-
-  window.addEventListener("DOMContentLoaded", function () {
-    console.log("Loaded");
-  });
 }
 
 // movie hover//
@@ -130,7 +124,83 @@ wrappperDivEl.forEach((el) => {
   });
 });
 
+// click wrapper div to = MOVIE //
+const movie_page = function () {
+  const movie_pageEl = document.querySelector(".movie_page");
+  const landing_MovieEl = document.querySelector(".landing_movie");
+  const trending_divEL = document.querySelector(".trending");
+  const section_TwoEl = document.querySelector(".two");
+  const parentELs = document.querySelectorAll(".cover_div");
+  window.addEventListener("DOMContentLoaded", function () {
+    parentELs.forEach((el) => {
+      el.addEventListener("click", function (event) {
+        const markup = `<div class="wrapper-movie ">
+        <div class="single_movie">
+          <div class="stars">
+            <div class="ratings_stars">
+              <i class="fa-solid fa-star fa-lg"></i>
+              <i class="fa-solid fa-star fa-lg"></i>
+              <i class="fa-solid fa-star fa-lg"></i>
+              <i class="fa-solid fa-star-half-stroke fa-lg"></i>
+              <i class="fa-regular fa-star fa-lg"></i>
+            </div>
+            <div class="review">
+              <p>6.6 0f 10(227 reviews)</p>
+            </div>
+          </div>
+
+          <div class="movie-description">
+            <h4 class="movie_title">The Professor</h4>
+            <div class="inner_landing_div movies">
+              <h4 class="clicked format">HD</h4>
+              <h4 class="active p_guidance">PG-13</h4>
+
+              <div class="rating">
+                <i class="fa-solid fa-star" style="color: #ffffff"></i>
+                <h4 class="movie_rating">7.7</h4>
+              </div>
+              <h4 class="year">2023</h4>
+              <h4 class="duration">114 min</h4>
+            </div>
+            <p class="movie_synopsis">
+              After learning about his terminal diagnosis, a college professor
+              decides to live his life to the fullest by drinking, smoking and
+            </p>
+            <div class="movie_infos">
+              <h5 class="movie_info">Type:</h5>
+              <h5 class="movie_info">Movie</h5>
+              <h5 class="movie_info">Country:</h5>
+              <h5 class="movie_info">United States</h5>
+              <h5 class="movie_info">Genre:</h5>
+              <h5 class="movie_info">Drama, Comedy, Family</h5>
+              <h5 class="movie_info">Release Date:</h5>
+              <h5 class="movie_info">jul 24, 2024</h5>
+              <h5 class="movie_info">Director:</h5>
+              <h5 class="movie_info">Johnny Depp</h5>
+              <h5 class="movie_info">Cast:</h5>
+              <h5 class="movie_info">
+                Johnny Depp, Rosemarie DeWitt, Danny Huston, Zoey Deutch, Ron
+                Livingstone, Linda Emond
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>`;
+        section_TwoEl.innerHTML = "";
+        navbarEL.classList.toggle("sticky_movie");
+
+        landing_MovieEl.classList.toggle("hidden");
+        trending_divEL.classList.toggle("hidden");
+        movie_pageEl.classList.toggle("hidden");
+        section_TwoEl.insertAdjacentHTML("afterbegin", markup);
+        console.log(event.target);
+      });
+    });
+  });
+};
+
 // Event Listiner//
 toggelMenu();
 scroll_naavbar();
 observeDivs();
+movie_page();
